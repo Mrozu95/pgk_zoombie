cb219185566636dcda13fdaf6b04caf22a215b4f
195739 Włodarczyk Maciej <195739@edu.p.lodz.pl>
2016-10-13 18:17:33 +0200

    Monetki, woda
    Dodanie monet, wody-spowalniacza, liczydelka monet i cos ala predkosci,

---
+++ pgk_zoombie/Assets/Scripts/Coin_script.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Coin_script : MonoBehaviour {
+
+
+	// Update is called once per frame
+    // obracanie sie diamencikow/monetek
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+
+}
--- pgk_zoombie/Assets/Scripts/Lvl_1_player_movment.cs
+++ pgk_zoombie/Assets/Scripts/Lvl_1_player_movment.cs
@@ -1,23 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
+
 
 public class Lvl_1_player_movment : MonoBehaviour {
 
     Rigidbody rb;
     public float speed;
+    public Text countText;
+    public Text speedText;
+    public int count;
 
-	// Use this for initialization
-	void Start ()
+    // Dodanie monety
+    public void SetCountText()
     {
-        set_speed(30);
+        countText.text = "Monety : " + count.ToString();
+    }
+    public void SetSpeedText()
+    {
+        speedText.text = "Speed : " + rb.velocity.ToString();
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        set_speed(10);
         rb = GetComponent<Rigidbody>();
+        count = 0; // początkowa ilośc monetek
+        SetCountText();
+        SetSpeedText();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
         movment();
-	}
+        SetSpeedText();
+       
+    }
 
     void set_speed(float speed)
     {
@@ -33,6 +53,37 @@ public class Lvl_1_player_movment : MonoBehaviour {
 
         Vector3 movment = new Vector3(moveHorizontal, 0, moveVertical);
 
-        rb.AddForce(movment * speed);
+        Vector3 compare = new Vector3(0.0f, 0.0f, 5.0f); // do stworzenia predksoci maxymalnej ale nie dziala to za bardzo
+        
+
+        if (compare.z - movment.z > 0)
+        {
+            rb.AddForce(movment * speed);
+        }
+
+
+        
+    }
+    //znikanie monet
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+        
+    }
+
+    // woda - spowalniacz
+   void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water"))
+        {
+            //Vector3 temp = new Vector3(0.00f,0.00f,0.15f);
+            rb.velocity = rb.velocity * 0.975f;
+        }
+           
     }
 }
