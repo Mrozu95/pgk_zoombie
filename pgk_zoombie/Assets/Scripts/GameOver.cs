d6d9c2a1ce9c11326115f44f446a1392428661c5
Daniel Drewniak <danieldrewniak95@gmail.com>
2016-11-09 15:43:08 +0100

    GameOver
    Dodałem ekran końca gry. Gdy hp gracza spadnie do 0 pojawia się na 5
    sekund czarny ekran z napisem "Game Over!". Nie jestem pewien czemu po
    ponownym wczytaniu poziomu scena jest jakby ciemniejsza :v

---
+++ pgk_zoombie/Assets/Scripts/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour
+{
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
+
+	void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Health.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+            restartTimer += Time.deltaTime;
+
+            if(restartTimer >= restartDelay)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+	}
+}
