15f8cbe87d5ae87a798b4bc4526907a12e04b38b
Mrozu95 <mrozu995@gmail.com>
2016-11-28 17:33:02 +0100

    Teren
    Podmienona trasa, ciezarowka zabija zombiaki

---
+++ pgk_zoombie/Assets/Scripts/TruckMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TruckMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(this.transform.position.x + 0.61f, this.transform.position.y, this.transform.position.z);
+	}
+}
--- pgk_zoombie/Assets/Scripts/Zombie_Movement.cs
+++ pgk_zoombie/Assets/Scripts/Zombie_Movement.cs
@@ -1,26 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Zoombie_movment : MonoBehaviour {
+public class Zombie_Movement : MonoBehaviour {
 
     Rigidbody rb;
     public Transform player;
     private float zoombie_speed; //pseudo szybkosc, jest to maksymalna różnica odległości od gracza, ale im da sie wieksza to poruszaja sie szybciej
     private Vector3 direction;
     private bool stop;
-    
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         //rb = new Rigidbody();
         rb = GetComponent<Rigidbody>(); // tak dziala nasz movment z new rigidbody nie dzialalo
         zoombie_speed = 0.15f;
         stop = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if(stop == false)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stop == false)
         {
             movment();
         }
@@ -28,9 +30,9 @@ public class Zoombie_movment : MonoBehaviour {
         {
             this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, 0.07f);
             haltnichtsitzen();
-            
-        }      
-	}
+
+        }
+    }
 
     //pausa po hicie
     public IEnumerator haltnichtsitzen()
@@ -68,8 +70,12 @@ public class Zoombie_movment : MonoBehaviour {
     public bool OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {     
+        {
             stop = true;
+        }
+        if (collision.gameObject.CompareTag("Truck"))
+        {
+            this.gameObject.SetActive(false);
         }
         return stop;
     }
