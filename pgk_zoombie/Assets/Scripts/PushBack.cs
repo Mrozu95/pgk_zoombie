a53c34025ef1e30fa7fc4c390817d8f8465efc41
Daniel Drewniak <danieldrewniak95@gmail.com>
2017-01-18 13:33:52 +0100

    Odepchnięcie
    Kolejny efekt do skilla.

---
+++ pgk_zoombie/Assets/Scripts/PushBack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBack : MonoBehaviour {
+
+    public Transform target;
+    private Transform myTransform;
+    public Vector3 offsetToTarget = new Vector3(0.0f, 0.0f, 0.0f);
+
+    Animator anim;
+    int coins_count;
+
+    void Start()
+    {
+        myTransform = transform;
+        anim = GetComponent<Animator>();
+        coins_count = Lvl_1_player_movment.coins_count;
+    }
+
+    void Update()
+    {
+        myTransform.position = target.position + offsetToTarget;
+        coins_count = Lvl_1_player_movment.coins_count;
+        teleporterControl();
+    }
+
+    public void teleporterControl()
+    {
+        if ((Input.GetKeyDown(KeyCode.X) && coins_count >= 3) || (Input.GetKeyDown(KeyCode.Period) && coins_count >= 3)) // sprawdzanie przycisku C
+        {
+            anim.SetTrigger("push");
+        }
+    }
+}
